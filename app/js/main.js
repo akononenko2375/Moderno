a9d770810e7140cdce404f3c5df7fe2299d8e3ea
Aleksandr Kononenko <sawa2375@gmail.com>
2020-09-13 22:28:26 +0300

    added new-items.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-	let mixer = mixitup('.products__inner-box');
+
 
 	$(".rate-star").rateYo({
 		rating: 5,
@@ -8,11 +8,25 @@ $(function () {
 		readOnly: true
 	});
 
+
 	$('.product-slider__inner').slick({
 		slidesToShow: 4,
 		slidesToScroll: 4,
 		dots: true,
 		arrows: false,
 	});
+
+	$(".js-range-slider").ionRangeSlider({
+		type: "double",
+		grid: false,
+		min: 0,
+		max: 1000,
+		from: 0,
+		to: 600,
+		prefix: "$"
+	});
+
+	let mixer = mixitup('.products__inner-box');
+
 
 });
